bcb40b20b398c04e7fee4fc7a56fe27d50031e42
ch <2630601704@qq.com>
2018-11-09 17:23:08 +0800

    Changes to be committed: 	renamed:    config.csv -> Observer/src/config.csv 	modified:   Observer/src/observer/ConcreteObserver.java 	new file:   config.epf

--- Observer/src/observer/ConcreteObserver.java
+++ Observer/src/observer/ConcreteObserver.java
@@ -22,8 +22,9 @@ public class ConcreteObserver implements Observer {
 	 * @see observer.Observer#update()
 	 */
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	public void update() { 
+		// TODO Auto-generated method estub
+		
 
 	}
 
